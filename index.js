6ee3b4289cef8ff1503908793f825ad59bbb9dcc
RedietZeraiHadera <redietzeraihadderra@gmail.com>
2023-05-22 19:10:49 +0300

    commit

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let childFruit= document.createElement("li");
 childFruit.innerHTML = 'Apple'
 document.getElementById('fruList').appendChild(childFruit);
 let childVeg= document.createElement("li"); 
-childFruit.innerHTML = 'Eggplant'
-document.getElementById('vegList').appendChild(childFruit);
+childVeg.innerHTML = 'Eggplant'
+document.getElementById('vegList').appendChild(childVeg);
 document.getElementById("h").style.textTransform='uppercase';
 document.getElementById("h2").style.textTransform='uppercase';
 
