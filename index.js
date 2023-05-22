3b0bdfa55b7a7524c9a27c5c519b07b9486de117
RedietZeraiHadera <redietzeraihadderra@gmail.com>
2023-05-22 18:54:00 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("doc").style.backgroundColor='silver'
+document.getElementById("title").style.color='green'
+let childFruit= document.createElement("li"); 
+childFruit.innerHTML = 'Apple'
+document.getElementById('fruList').appendChild(childFruit);
+let childVeg= document.createElement("li"); 
+childFruit.innerHTML = 'Eggplant'
+document.getElementById('vegList').appendChild(childFruit);
+document.getElementById("h").style.textTransform='uppercase';
+document.getElementById("h2").style.textTransform='uppercase';
+
